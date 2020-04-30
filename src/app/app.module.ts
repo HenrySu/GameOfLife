999445e76e1b7a90f02da337ada785dc7c196c6b
Henry SU <hsu2@slb.com>
2020-04-30 11:40:26 +0800

    move game board to game folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
-import { GameBoardComponent } from './game-board/game-board.component';
+import { GameBoardComponent } from './game/game-board/game-board.component';
 
 @NgModule({
   declarations: [
