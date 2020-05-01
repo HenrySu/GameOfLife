03776548d106c0bbba36a4c68055187f949f031c
Henry SU <hsu2@slb.com>
2020-05-01 21:53:48 +0800

    add submit

--- src/app/game/components/game-setting/game-setting.component.ts
+++ src/app/game/components/game-setting/game-setting.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
@@ -7,18 +7,25 @@ import { FormBuilder, Validators } from "@angular/forms";
   styleUrls: ['./game-setting.component.scss']
 })
 export class GameSettingComponent implements OnInit {
+  @Output() gameSettingChanged = new EventEmitter();
+
   alivePercentage: number = 50;
+
   settingForm = this.fb.group({
     boardSize: this.fb.group({
       rowCount: 100,
       columnCount: 100
     }),
     alivePercentage: this.alivePercentage,
-    evolutionTimeInterval: 1000
+    evolutionTimeInterval: 1040
   });
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.gameSettingChanged.emit(this.settingForm.value);
   }
 }
